bde0b4c027788fbe6f1ff5e04c3f4a4656a49b11
Matt <matthias-baumann@outlook.com>
2018-10-22 10:05:49 +0200

    Add sample list handler
    A handler which will include a sample list with sample data. This
    ensures that the student will never be unsure about the status of the
    service, just because "nothing" is stored in the database.

--- purchase-list-service/src/main/java/ch/hsr/apparch/purchaselist/Application.java
+++ purchase-list-service/src/main/java/ch/hsr/apparch/purchaselist/Application.java
@@ -1,12 +1,28 @@
 package ch.hsr.apparch.purchaselist;
 
+import ch.hsr.apparch.purchaselist.model.PurchaseList;
+import ch.hsr.apparch.purchaselist.repository.PurchaseListItemRepository;
+import ch.hsr.apparch.purchaselist.repository.PurchaseListRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.Collections;
 
 @SpringBootApplication
 public class Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner insertSampleData(PurchaseListRepository lists, PurchaseListItemRepository listItems) {
+        return args -> {
+            PurchaseList list = lists.save(new PurchaseList("Sample List", LocalDate.of(2100, 10, 18), Collections.emptyList()));
+        };
     }
 }
