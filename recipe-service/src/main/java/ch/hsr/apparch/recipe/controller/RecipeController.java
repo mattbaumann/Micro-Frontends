e88594bfdb1d572b3c68af61350e7c416c8124d8
Matt <matthias-baumann@outlook.com>
2018-10-26 19:01:23 +0200

    Modernizes recipe controller
    Recipe controller has new, rest-orientend URL scheme. This scheme is
    uniform across all services.

--- recipe-service/src/main/java/ch/hsr/apparch/recipe/controller/RecipeController.java
+++ recipe-service/src/main/java/ch/hsr/apparch/recipe/controller/RecipeController.java
@@ -1,12 +1,17 @@
 package ch.hsr.apparch.recipe.controller;
 
+import ch.hsr.apparch.recipe.model.Category;
 import ch.hsr.apparch.recipe.model.Recipe;
 import ch.hsr.apparch.recipe.repository.CategoryRepository;
 import ch.hsr.apparch.recipe.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/recipe")
@@ -30,22 +35,40 @@ public class RecipeController {
         return "recipe/list";
     }
 
-    @GetMapping({"/edit", "/edit/{id}"})
-    public String edit(
-            @PathVariable(value = "id", required = false) final Long id,
-            final Model model) {
-        if (id != null)
-            model.addAttribute("model", recipeRepository.findById(id));
-        else
-            model.addAttribute("model", new Recipe());
+    @GetMapping({"/edit", "/{id}/edit"})
+    public String edit(@PathVariable(value = "id", required = false) final Optional<Long> id,
+                       final Model model) {
+        model.addAttribute("model", id.flatMap(recipeRepository::findById).orElse(new Recipe()));
         model.addAttribute("categories", categoryRepository.findAll());
         return "recipe/edit";
     }
 
-    @PostMapping("/update")
-    public String update(@RequestParam(value = "id", required = false) final Long id,
+    @PostMapping("{id}/update")
+    public String update(@PathVariable("id") final long id,
                          @RequestParam("name") final String name,
-                         @RequestParam("category") final int category_id) {
+                         @RequestParam("category") final long categoryId) {
+
+        Category category = categoryRepository.findById(categoryId).orElseThrow(ResourceNotFoundException::new);
+        recipeRepository.findById(id)
+                .map(recipe -> recipe.setName(name).setCategory(category))
+                .map(recipeRepository::save);
+        return REDIRECT_CONTROLLER_LIST_VIEW;
+    }
+
+    @PostMapping("/add")
+    public String update(@RequestParam("name") final String name,
+                         @RequestParam("category") final long category_id) {
+
+        categoryRepository.findById(category_id)
+                .map(category -> new Recipe(name, Collections.emptyList(), Collections.emptyList(), category))
+                .map(recipeRepository::save)
+                .orElseThrow(ResourceNotFoundException::new);
+        return REDIRECT_CONTROLLER_LIST_VIEW;
+    }
+
+    @GetMapping("{id}/delete")
+    public String delete(@PathVariable("id") final long id) {
+        recipeRepository.deleteById(id);
         return REDIRECT_CONTROLLER_LIST_VIEW;
     }
 }
