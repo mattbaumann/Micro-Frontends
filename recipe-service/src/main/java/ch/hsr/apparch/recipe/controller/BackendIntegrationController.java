c3f38785bb89ad74a50d9611aae8d658baf88ca4
Matt <matthias-baumann@outlook.com>
2018-10-28 15:09:22 +0100

    Prototype Backend Integration Controller
    Allows to load sites from other services and presents them as his own.
    That way he impersonates the other services and the browser plays his
    role in the game.

--- recipe-service/src/main/java/ch/hsr/apparch/recipe/controller/BackendIntegrationController.java
+++ recipe-service/src/main/java/ch/hsr/apparch/recipe/controller/BackendIntegrationController.java
@@ -1,18 +1,26 @@
 package ch.hsr.apparch.recipe.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BackendIntegrationController {
 
+    private static final String TARGET_SITE_KEY = "targetSite";
+    private static final String TARGET_ELEMENT_KEY = "targetElement";
+
     @GetMapping("/purchaseList/list")
-    public String listPurchaseLists() {
+    public String listPurchaseLists(Model model) {
+        model.addAttribute(TARGET_SITE_KEY, "http://localhost:9602/purchaseList/list");
+        model.addAttribute(TARGET_ELEMENT_KEY, "table");
         return "purchase-list/list";
     }
 
     @GetMapping("purchaseList/edit")
-    public String editPurchaseList() {
-        return
+    public String editPurchaseList(Model model) {
+        model.addAttribute(TARGET_SITE_KEY, "http://localhost:9602/purchaseList/edit");
+        model.addAttribute(TARGET_ELEMENT_KEY, "form");
+        return "purchase-list/list";
     }
 }
