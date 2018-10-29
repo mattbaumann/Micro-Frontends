1a5702743b33b60fd3b7cc5a23c7b2ad9d5b02ca
Matt <matthias-baumann@outlook.com>
2018-10-29 14:11:18 +0100

    Add Backend Integration of the files
    The Backend integrates the other services by rendering their pages
    into the page of the common backend.
    
    Currently working:
    * Loading pages
    * Displaying the pages as if they were core service's pages
    * Sending services the signal, that their pages will be backend-
    integrated and their links must be reworked in order to be workable.

--- recipe-service/src/main/java/ch/hsr/apparch/recipe/controller/SSFragmentCompositionController.java
+++ recipe-service/src/main/java/ch/hsr/apparch/recipe/controller/SSFragmentCompositionController.java
@@ -1,28 +1,36 @@
 package ch.hsr.apparch.recipe.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 @RequestMapping("/SSFragmentCompositionController")
 public class SSFragmentCompositionController {
 
+    private static final Logger LOGGER = LogManager.getLogger("Application");
+    private static final AntPathMatcher MATCHER = new AntPathMatcher();
+
     private static final String TARGET_SITE_KEY = "targetSite";
     private static final String TARGET_ELEMENT_KEY = "targetElement";
 
-    @GetMapping("/purchaseList/list")
-    public String listPurchaseLists(Model model) {
-        model.addAttribute(TARGET_SITE_KEY, "http://localhost:9602/purchaseList/list");
-        model.addAttribute(TARGET_ELEMENT_KEY, "table");
-        return "serverSideFragmentComposition";
+    private final int port;
+
+    @Autowired
+    public SSFragmentCompositionController(@Value("${server.port}") int port) {
+        this.port = port;
     }
 
-    @GetMapping("purchaseList/edit")
-    public String editPurchaseList(Model model) {
-        model.addAttribute(TARGET_SITE_KEY, "http://localhost:9602/purchaseList/edit");
-        model.addAttribute(TARGET_ELEMENT_KEY, "form");
-        return "serverSideFragmentComposition";
+    private static String matchPath(HttpServletRequest request) {
+        String bestMatchPattern = (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        return MATCHER.extractPathWithinPattern(bestMatchPattern, request.getRequestURI());
     }
 }
