3bec3d9f9e62cd0f44defa9eefd37f21f4bb1db2
Matt <mattbaumann@users.noreply.github.com>
2018-11-07 10:17:35 +0100

    Add CORS headers to recipe service
    In modern browsers is CORS enabled by default and has to be mitigated
    in the webservice to support Javascript requests.

--- recipe-service/src/main/java/ch/hsr/apparch/recipe/configuration/WebMVCConfiguration.java
+++ recipe-service/src/main/java/ch/hsr/apparch/recipe/configuration/WebMVCConfiguration.java
@@ -1,10 +1,8 @@
 package ch.hsr.apparch.recipe.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 import org.thymeleaf.templateresolver.UrlTemplateResolver;
 
