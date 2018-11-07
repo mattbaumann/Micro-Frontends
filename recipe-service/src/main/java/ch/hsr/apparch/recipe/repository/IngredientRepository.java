f0c8622a103fccb41be91ada143576e6023ab6c3
Matt <mattbaumann@users.noreply.github.com>
2018-11-07 10:17:35 +0100

    Add CORS headers to recipe service
    In modern browsers is CORS enabled by default and has to be mitigated
    in the webservice to support Javascript requests.

--- recipe-service/src/main/java/ch/hsr/apparch/recipe/repository/IngredientRepository.java
+++ recipe-service/src/main/java/ch/hsr/apparch/recipe/repository/IngredientRepository.java
@@ -4,9 +4,11 @@ import ch.hsr.apparch.recipe.model.Ingredient;
 import ch.hsr.apparch.recipe.model.Instruction;
 import ch.hsr.apparch.recipe.model.Recipe;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Collection;
 
+@CrossOrigin
 public interface IngredientRepository extends PagingAndSortingRepository<Ingredient, Long> {
 
     Collection<Ingredient> findAllByRecipe(Recipe recipe);
