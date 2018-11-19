9d02cac4bc10687f6007c2288f47ce1a5e5fe7a0
Matt <mattbaumann@users.noreply.github.com>
2018-11-19 23:36:27 +0100

    Update Backend Tests with fresh tests

--- services/kitchen-device-service/src/main/java/ch/hsr/apparch/kitchenDevices/Application.java
+++ services/kitchen-device-service/src/main/java/ch/hsr/apparch/kitchenDevices/Application.java
@@ -3,13 +3,12 @@ package ch.hsr.apparch.kitchenDevices;
 import ch.hsr.apparch.kitchenDevices.model.KitchenDevice;
 import ch.hsr.apparch.kitchenDevices.repository.KitchenDeviceRepository;
 import lombok.extern.log4j.Log4j2;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,6 +29,7 @@ public class Application {
     }
 
     @Bean
+    @Profile("dev")
     public CommandLineRunner insertSampleData(KitchenDeviceRepository lists) {
         return args -> {
             Random random = new Random();
@@ -43,7 +43,8 @@ public class Application {
     }
 
     @Bean
+    @Profile("dev")
     public CommandLineRunner logPortAtStartup(@Value("${server.port}") int port) {
-        return args -> LOGGER.info("Spring Server 'Recipe' is running under port {}", port);
+        return args -> LOGGER.info("Spring Server 'Kitchen Device' is running under port {}", port);
     }
 }
