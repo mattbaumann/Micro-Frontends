9c399528c19977211abebd9585bfee1782f526c7
Matt <mattbaumann@users.noreply.github.com>
2018-11-20 15:23:47 +0100

    Separate Frontends into subfrontends

--- frontend-composition/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import 'core-js'
-import 'react-app-polyfill/ie9';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'semantic-ui-css/semantic.min.css';
-import App from './ui/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
